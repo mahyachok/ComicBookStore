1e2ade57cbf43d531a3551ae8f3b79a8cfd36685
mahyachok <mahyachok@SAMMAYAK0F5D>
2024-04-17 10:37:18 -0400

    added the database

--- ComicBookStore/Database.cs
+++ ComicBookStore/Database.cs
@@ -7,15 +7,58 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
+
+/*
+ Databasse contant 
+
+database name : ComicBookDatabase
+
+
+first
+table name : ComicBook
+fields : SeriesTitle, IssueNumber, CoverImage, Author, Illustrator, Price, VarientCover, Reprint, 
+
+
+
+//maybe ill add these one later Publisher, Genre, ReleaseDate, Stock
+
+
+second table
+
+table: Store
+
+fields: StoreName, StoreLocation, StoreInventory
+
+ 
+ 
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 namespace ComicBookStore
 {
+
     internal class Database
     {
 
         OleDbConnection myConnection;
         OleDbDataAdapter myDataAdapter;
-        DataSet studentDataSet, courseDataSet;
-        DataTable courseTable;
+        DataSet courseDataSet;
+        DataTable comicTable;
         BindingSource myBindingSource;
         string strSQL;
 
@@ -25,28 +68,28 @@ namespace ComicBookStore
 
 
         string queryString;
-        string connectString = "provider=Microsoft.ACE.OLEDB.12.0; Data Source=../../ComicDatabase.accdb;";
+        string connectString = "provider=Microsoft.ACE.OLEDB.12.0; Data Source=../../ComicbookDatabase.accdb;";
 
 
 
 
-        public   DataTable GetDatabaseInfo(string strSQL = "SELECT * FROM ComicDatabase")
+        public   DataTable GetDatabaseInfo(string strSQL = "SELECT * FROM ComicBook")
         {
-            myConnection = new OleDbConnection("provider=Microsoft.ACE.OLEDB.12.0;Data Source=ComicDatabase.accdb;");
+            myConnection = new OleDbConnection("provider=Microsoft.ACE.OLEDB.12.0;Data Source=ComicbookDatabase.accdb;");
             
 
             myDataAdapter = new OleDbDataAdapter(strSQL, myConnection);
-            courseDataSet = new DataSet("ComicTable");
-            myDataAdapter.Fill(courseDataSet, "ComicTable");
+            courseDataSet = new DataSet("Comic");
+            myDataAdapter.Fill(courseDataSet, "Comic");
 
             // Display the data from the query in the ComboBox control.
 
-            courseTable = courseDataSet.Tables["ComicTable"];
+            comicTable = courseDataSet.Tables["Comic"];
             
 
            
 
-            return courseTable;
+            return comicTable;
 
 
 
