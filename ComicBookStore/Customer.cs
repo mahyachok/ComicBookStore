5e3a09d1ce15ffd27f1501dc1ba36917a0daaa76
amanbenoy <amanbenoy91@gmail.com>
2024-04-17 13:48:52 -0400

    created emploee and customer classes

---
+++ ComicBookStore/Customer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComicBookStore
+{
+    internal class Customer
+    {
+        private string username;
+        private string password;
+        private string name;
+        private List<Comicbook> comicBookCollection;
+
+
+        public Customer(string username, string password, string name)
+        {
+            this.username = username;
+            this.password = password;
+            this.name = name;
+        }
+
+        public string Username
+        { 
+            get { return username; } 
+            set { username = value; }
+        }
+        public string Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+    }
+}
---
+++ ComicBookStore/Employee.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComicBookStore
+{
+    internal class Employee
+    {
+        private string username;
+        private string password;
+        private string name;
+        private double salary;
+
+        public Employee(string username, string password, string name, double salary)
+        {
+            this.username = username;
+            this.password = password;
+            this.name = name;
+            this.salary = salary;
+        }
+
+        public string Username
+        {
+            get { return username; }
+            set { username = value; }
+        }
+
+        public string Password
+        { get { return password; } set { password = value; } }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public double Salary
+        {
+            get { return salary; }
+            set { salary = value; }
+        }
+    }
+}
+
