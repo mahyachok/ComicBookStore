733de4b6803cb70676b7eb5cc3d8a4bf3accdebb
amanbenoy <amanbenoy91@gmail.com>
2024-04-22 14:04:01 -0400

    login form and controller class

---
+++ ComicBookStore/ComicbookController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComicBookStore
+{
+    internal class ComicbookController
+    {
+
+        OleDbConnection myConnection;
+        OleDbDataAdapter myDataAdapter;
+        DataSet myDataSet;
+        string strSQL;
+
+
+        public ComicbookController()
+        {
+            myConnection = new OleDbConnection("provider=Microsoft.ACE.OLEDB.12.0;Data Source=ComicBookStore.accdb;");
+        }
+    }
+}
---
+++ ComicBookStore/frmLogin.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace ComicBookStore
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grpCust = new System.Windows.Forms.GroupBox();
+            this.grpEmp = new System.Windows.Forms.GroupBox();
+            this.txtCustUser = new System.Windows.Forms.TextBox();
+            this.txtCustPass = new System.Windows.Forms.TextBox();
+            this.txtEmpUser = new System.Windows.Forms.TextBox();
+            this.txtEmpPass = new System.Windows.Forms.TextBox();
+            this.lblUser = new System.Windows.Forms.Label();
+            this.lblPass = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnEmpLogin = new System.Windows.Forms.Button();
+            this.btnCustLogin = new System.Windows.Forms.Button();
+            this.btnCreateCust = new System.Windows.Forms.Button();
+            this.btnCreateEmp = new System.Windows.Forms.Button();
+            this.grpCust.SuspendLayout();
+            this.grpEmp.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grpCust
+            // 
+            this.grpCust.Controls.Add(this.btnCustLogin);
+            this.grpCust.Controls.Add(this.lblPass);
+            this.grpCust.Controls.Add(this.lblUser);
+            this.grpCust.Controls.Add(this.txtCustPass);
+            this.grpCust.Controls.Add(this.txtCustUser);
+            this.grpCust.Location = new System.Drawing.Point(49, 62);
+            this.grpCust.Name = "grpCust";
+            this.grpCust.Size = new System.Drawing.Size(268, 223);
+            this.grpCust.TabIndex = 0;
+            this.grpCust.TabStop = false;
+            this.grpCust.Text = "Customer Login";
+            // 
+            // grpEmp
+            // 
+            this.grpEmp.Controls.Add(this.btnEmpLogin);
+            this.grpEmp.Controls.Add(this.label2);
+            this.grpEmp.Controls.Add(this.label1);
+            this.grpEmp.Controls.Add(this.txtEmpPass);
+            this.grpEmp.Controls.Add(this.txtEmpUser);
+            this.grpEmp.Location = new System.Drawing.Point(419, 64);
+            this.grpEmp.Name = "grpEmp";
+            this.grpEmp.Size = new System.Drawing.Size(265, 220);
+            this.grpEmp.TabIndex = 1;
+            this.grpEmp.TabStop = false;
+            this.grpEmp.Text = "Employee Login";
+            // 
+            // txtCustUser
+            // 
+            this.txtCustUser.Location = new System.Drawing.Point(88, 55);
+            this.txtCustUser.Name = "txtCustUser";
+            this.txtCustUser.Size = new System.Drawing.Size(100, 20);
+            this.txtCustUser.TabIndex = 0;
+            // 
+            // txtCustPass
+            // 
+            this.txtCustPass.Location = new System.Drawing.Point(88, 98);
+            this.txtCustPass.Name = "txtCustPass";
+            this.txtCustPass.Size = new System.Drawing.Size(100, 20);
+            this.txtCustPass.TabIndex = 0;
+            // 
+            // txtEmpUser
+            // 
+            this.txtEmpUser.Location = new System.Drawing.Point(95, 66);
+            this.txtEmpUser.Name = "txtEmpUser";
+            this.txtEmpUser.Size = new System.Drawing.Size(100, 20);
+            this.txtEmpUser.TabIndex = 0;
+            // 
+            // txtEmpPass
+            // 
+            this.txtEmpPass.Location = new System.Drawing.Point(95, 109);
+            this.txtEmpPass.Name = "txtEmpPass";
+            this.txtEmpPass.Size = new System.Drawing.Size(100, 20);
+            this.txtEmpPass.TabIndex = 0;
+            // 
+            // lblUser
+            // 
+            this.lblUser.AutoSize = true;
+            this.lblUser.Location = new System.Drawing.Point(24, 55);
+            this.lblUser.Name = "lblUser";
+            this.lblUser.Size = new System.Drawing.Size(58, 13);
+            this.lblUser.TabIndex = 1;
+            this.lblUser.Text = "Username:";
+            // 
+            // lblPass
+            // 
+            this.lblPass.AutoSize = true;
+            this.lblPass.Location = new System.Drawing.Point(26, 101);
+            this.lblPass.Name = "lblPass";
+            this.lblPass.Size = new System.Drawing.Size(56, 13);
+            this.lblPass.TabIndex = 1;
+            this.lblPass.Text = "Password:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Username:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 112);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Password:";
+            // 
+            // btnEmpLogin
+            // 
+            this.btnEmpLogin.Location = new System.Drawing.Point(120, 174);
+            this.btnEmpLogin.Name = "btnEmpLogin";
+            this.btnEmpLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnEmpLogin.TabIndex = 2;
+            this.btnEmpLogin.Text = "Login";
+            this.btnEmpLogin.UseVisualStyleBackColor = true;
+            // 
+            // btnCustLogin
+            // 
+            this.btnCustLogin.Location = new System.Drawing.Point(143, 176);
+            this.btnCustLogin.Name = "btnCustLogin";
+            this.btnCustLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnCustLogin.TabIndex = 2;
+            this.btnCustLogin.Text = "Login";
+            this.btnCustLogin.UseVisualStyleBackColor = true;
+            // 
+            // btnCreateCust
+            // 
+            this.btnCreateCust.Location = new System.Drawing.Point(192, 362);
+            this.btnCreateCust.Name = "btnCreateCust";
+            this.btnCreateCust.Size = new System.Drawing.Size(145, 51);
+            this.btnCreateCust.TabIndex = 2;
+            this.btnCreateCust.Text = "Create a New Account (Customer)";
+            this.btnCreateCust.UseVisualStyleBackColor = true;
+            // 
+            // btnCreateEmp
+            // 
+            this.btnCreateEmp.Location = new System.Drawing.Point(469, 362);
+            this.btnCreateEmp.Name = "btnCreateEmp";
+            this.btnCreateEmp.Size = new System.Drawing.Size(145, 51);
+            this.btnCreateEmp.TabIndex = 2;
+            this.btnCreateEmp.Text = "Create a New Account (Employee)";
+            this.btnCreateEmp.UseVisualStyleBackColor = true;
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCreateEmp);
+            this.Controls.Add(this.btnCreateCust);
+            this.Controls.Add(this.grpEmp);
+            this.Controls.Add(this.grpCust);
+            this.Name = "frmLogin";
+            this.Text = "frmLogin";
+            this.grpCust.ResumeLayout(false);
+            this.grpCust.PerformLayout();
+            this.grpEmp.ResumeLayout(false);
+            this.grpEmp.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grpCust;
+        private System.Windows.Forms.Button btnCustLogin;
+        private System.Windows.Forms.Label lblPass;
+        private System.Windows.Forms.Label lblUser;
+        private System.Windows.Forms.TextBox txtCustPass;
+        private System.Windows.Forms.TextBox txtCustUser;
+        private System.Windows.Forms.GroupBox grpEmp;
+        private System.Windows.Forms.Button btnEmpLogin;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtEmpPass;
+        private System.Windows.Forms.TextBox txtEmpUser;
+        private System.Windows.Forms.Button btnCreateCust;
+        private System.Windows.Forms.Button btnCreateEmp;
+    }
+}
