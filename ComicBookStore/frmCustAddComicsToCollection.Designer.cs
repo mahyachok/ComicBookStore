fabd7ad6776c6ef8b4da0fad2a17968bb6746adf
mahyachok <mahyachok@SAMMAYAK0F5D>
2024-04-26 13:39:33 -0400

    added button and datagridview to custAddComics to collection

--- ComicBookStore/frmCustAddComicsToCollection.Designer.cs
+++ ComicBookStore/frmCustAddComicsToCollection.Designer.cs
@@ -29,24 +29,52 @@
         private void InitializeComponent()
         {
             this.propGridComicCollection = new System.Windows.Forms.PropertyGrid();
+            this.dgvComicbookSelection = new System.Windows.Forms.DataGridView();
+            this.btnCollection = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvComicbookSelection)).BeginInit();
             this.SuspendLayout();
             // 
             // propGridComicCollection
             // 
-            this.propGridComicCollection.Location = new System.Drawing.Point(163, 12);
+            this.propGridComicCollection.Location = new System.Drawing.Point(699, 42);
             this.propGridComicCollection.Name = "propGridComicCollection";
             this.propGridComicCollection.Size = new System.Drawing.Size(336, 325);
             this.propGridComicCollection.TabIndex = 0;
             this.propGridComicCollection.Click += new System.EventHandler(this.propGridComicCollection_Click);
             // 
+            // dgvComicbookSelection
+            // 
+            this.dgvComicbookSelection.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvComicbookSelection.Location = new System.Drawing.Point(103, 132);
+            this.dgvComicbookSelection.Name = "dgvComicbookSelection";
+            this.dgvComicbookSelection.RowHeadersWidth = 82;
+            this.dgvComicbookSelection.RowTemplate.Height = 33;
+            this.dgvComicbookSelection.Size = new System.Drawing.Size(487, 441);
+            this.dgvComicbookSelection.TabIndex = 1;
+            this.dgvComicbookSelection.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvComicbookSelection_CellContentClick);
+            // 
+            // btnCollection
+            // 
+            this.btnCollection.Location = new System.Drawing.Point(757, 447);
+            this.btnCollection.Name = "btnCollection";
+            this.btnCollection.Size = new System.Drawing.Size(155, 73);
+            this.btnCollection.TabIndex = 2;
+            this.btnCollection.Text = "Add to collection";
+            this.btnCollection.UseVisualStyleBackColor = true;
+            this.btnCollection.Click += new System.EventHandler(this.btnCollection_Click);
+            // 
             // frmCustAddComicsToCollection
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1135, 709);
+            this.Controls.Add(this.btnCollection);
+            this.Controls.Add(this.dgvComicbookSelection);
             this.Controls.Add(this.propGridComicCollection);
             this.Name = "frmCustAddComicsToCollection";
             this.Text = "frmCustAddComicsToCollection";
+            this.Load += new System.EventHandler(this.frmCustAddComicsToCollection_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvComicbookSelection)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -54,5 +82,7 @@
         #endregion
 
         private System.Windows.Forms.PropertyGrid propGridComicCollection;
+        private System.Windows.Forms.DataGridView dgvComicbookSelection;
+        private System.Windows.Forms.Button btnCollection;
     }
 }
