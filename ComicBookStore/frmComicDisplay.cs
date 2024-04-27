c25991d348ebdd0e9b15bb95a86383762d09c9d4
mahyachok <sammayak@gmail.com>
2024-04-26 20:55:06 -0400

    fixed the display

--- ComicBookStore/frmComicDisplay.cs
+++ ComicBookStore/frmComicDisplay.cs
@@ -1,11 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
+
 using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 using System.Windows.Forms;
 
 namespace ComicBookStore
@@ -14,15 +11,41 @@ namespace ComicBookStore
     {
         private ComicbookController controller;
         private FlowLayoutPanel flowLayoutPanel;
+        private Panel panel;
 
         public frmComicDisplay()
         {
             InitializeComponent();
             controller = new ComicbookController();
 
+
+           
+            /*
+            panel = new Panel();
+            panel.Dock = DockStyle.Fill;
+            Controls.Add(panel);
+            */
+
+
             flowLayoutPanel = new FlowLayoutPanel();
+
+            flowLayoutPanel.AutoSize = true;
+
+            flowLayoutPanel.AutoScroll = true;
+            flowLayoutPanel.FlowDirection = FlowDirection.TopDown;
+           
+
             flowLayoutPanel.Dock = DockStyle.Fill;
             Controls.Add(flowLayoutPanel);
+
+
+
+
+
+
+
+
+
         }
 
         private void frmComicDisplay_Load(object sender, EventArgs e)
@@ -32,8 +55,10 @@ namespace ComicBookStore
             foreach (DataRow row in dataTable.Rows)
             {
                 UserControl comicItem = new UserControl();
-                comicItem.Padding = new Padding(5); 
-                comicItem.Width = flowLayoutPanel.ClientSize.Width - 580; 
+
+
+                comicItem.Padding = new Padding(5);
+                comicItem.Width = flowLayoutPanel.ClientSize.Width - 100; 
 
                 TableLayoutPanel tableLayoutPanel = new TableLayoutPanel();
                 tableLayoutPanel.Dock = DockStyle.Fill;
